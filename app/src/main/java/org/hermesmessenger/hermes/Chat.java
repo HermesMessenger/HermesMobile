94038ce6ebd9980bfc6bd3d53fb470a603e367eb
theperkinrex <theperkinrex@gmail.com>
2019-01-19 18:23:50 +0100

    Little fix ** NOT READY FOR RELEASE **

--- app/src/main/java/org/hermesmessenger/hermes/Chat.java
+++ app/src/main/java/org/hermesmessenger/hermes/Chat.java
@@ -3,6 +3,7 @@ package org.hermesmessenger.hermes;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
